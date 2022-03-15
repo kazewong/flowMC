b7acacc4a1a8014493bc4815e87cbcb65a3c3cfe
Kaze Wong <kazewong.physics@gmail.com>
2022-03-15 14:41:26 -0400

    Add acceptance rate to kernel_test.py

--- test/kernel_test.py
+++ test/kernel_test.py
@@ -24,29 +24,31 @@ def mala_kernel(rng_key, logpdf, d_logpdf, position, log_prob, kernal_size=0.1):
 
     position = jnp.where(do_accept, proposal, position)
     log_prob = jnp.where(do_accept, proposal_log_prob, log_prob)
-    return position, log_prob
+    return position, log_prob, do_accept.astype(jnp.int8)
 
 mala_kernel_vec = jax.vmap(mala_kernel, in_axes=(0, None, None, 0, 0, None))
 
 def mala_sampler(rng_key, n_samples, logpdf, d_logpdf, initial_position, kernal_size=0.1):
 
     def mh_update_sol2(i, state):
-        key, positions, log_prob = state
+        key, positions, log_prob, acceptance = state
         _, key = jax.random.split(key)
-        new_position, new_log_prob = mala_kernel(key, logpdf, d_logpdf, positions[i-1], log_prob, kernal_size)
+        new_position, new_log_prob, accept_local = mala_kernel(key, logpdf, d_logpdf, positions[i-1], log_prob, kernal_size)
         positions=positions.at[i].set(new_position)
-        return (key, positions, new_log_prob)
+        acceptance += accept_local
+        return (key, positions, new_log_prob,acceptance)
 
 
     logp = logpdf(initial_position)
+    acceptance = jnp.zeros(logp.shape)
     all_positions = jnp.zeros((n_samples,)+initial_position.shape) + initial_position
-    initial_state = (rng_key,all_positions, logp)
-    rng_key, all_positions, log_prob = jax.lax.fori_loop(1, n_samples, 
-                                                 mh_update_sol2, 
-                                                 initial_state)
+    initial_state = (rng_key,all_positions, logp, acceptance)
+    rng_key, all_positions, log_prob, acceptance = jax.lax.fori_loop(1, n_samples, 
+                                                   mh_update_sol2, 
+                                                   initial_state)
     
     
-    return rng_key, all_positions, log_prob
+    return rng_key, all_positions, log_prob, acceptance/n_samples
 
 def dual_moon_pe(x):
     """
