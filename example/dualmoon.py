2a271109a8b06ffa89f5e170b980757fb638f630
kazewong <kazewong.physics@gmail.com>
2022-05-19 10:55:42 -0400

    Refactor functions import in dualmoon.py

--- example/dualmoon.py
+++ example/dualmoon.py
@@ -2,7 +2,8 @@ from nfsampler.nfmodel.realNVP import RealNVP
 from nfsampler.sampler.MALA import mala_sampler
 import jax
 import jax.numpy as jnp                # JAX NumPy
-from nfsampler.utils import Sampler, initialize_rng_keys
+from nfsampler.sampler import Sampler
+from nfsampler.utils.PRNG_keys import initialize_rng_keys
 from jax.scipy.special import logsumexp
 import numpy as np  
 
