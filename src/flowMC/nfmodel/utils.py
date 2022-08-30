Fix major slowdown from logging the loss with append. Please don't do this again LOL. We lost factor of 20

src/flowMC/nfmodel/utils.py
@@ -4,7 +4,6 @@ import jax.random as random            # JAX random
 from tqdm import tqdm
 
 
-@jax.jit(static_argnums=(0,))
 def train_step(model, batch, state, variables):
     def loss(params):
         log_det = model.apply({'params': params,'variables': variables}, batch, method=model.log_prob)
@@ -14,6 +13,8 @@ def train_step(model, batch, state, variables):
     value, grad = grad_fn(state.params)
     state = state.apply_gradients(grads=grad)
     return value, state
+
+train_step = jax.jit(train_step, static_argnums=(0,))
 
 def train_epoch(model, state, train_ds, batch_size, epoch, rng, variables):
     """Train for a single epoch."""
@@ -30,14 +31,14 @@ def train_epoch(model, state, train_ds, batch_size, epoch, rng, variables):
     return value, state
 
 def train_flow(rng, model, state, data, num_epochs, batch_size, variables):
-    loss_values = jnp.array([])
+    loss_values = jnp.zeros(num_epochs)
     for epoch in tqdm(range(1, num_epochs + 1),desc='Training NF',miniters=int(num_epochs/10)):
         # Use a separate PRNG key to permute image data during shuffling
         rng, input_rng = jax.random.split(rng)
         # Run an optimization step over a training batch
         value, state = train_epoch(model, state, data, batch_size, epoch, input_rng, variables)
         #print('Train loss: %.3f' % value)
-        loss_values = jnp.append(loss_values,value)
+        loss_values = loss_values.at[epoch].set(value)
     
     return rng, state, loss_values
 