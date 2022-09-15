Fix bugs related to normalizing flow proposal. Acceptance rate of GW BBH injection is about 1%

src/flowMC/nfmodel/rqSpline.py
@@ -53,9 +53,6 @@ class Scalar(nn.Module):
     def __call__(self, x):
         return self.scale, self.shift
 
-def bijector_fn(params: jnp.ndarray):
-    return distrax.RationalQuadraticSpline(params, range_min=-10., range_max=10.)
-
 def scalar_affine(params: jnp.ndarray):
     return distrax.ScalarAffine(scale=params[0],shift=params[1])
 
@@ -65,6 +62,7 @@ class RQSpline(nn.Module):
     num_layers: int
     hidden_size: Sequence[int]
     num_bins: int
+    spline_range: Sequence[float] = (-10., 10.)
 
     def setup(self):
         conditioner = []
@@ -80,6 +78,11 @@ class RQSpline(nn.Module):
         self.base_cov = self.variable('variables','base_cov',jnp.eye,(self.n_features))
 
         self.vmap_call = jax.jit(jax.vmap(self.__call__))
+        
+        def bijector_fn(params: jnp.ndarray):
+            return distrax.RationalQuadraticSpline(params, range_min=self.spline_range[0], range_max=self.spline_range[1])
+
+        self.bijector_fn = bijector_fn
 
     def make_flow(self):
         mask = (jnp.arange(0, self.n_features) % 2).astype(bool)
@@ -89,7 +92,7 @@ class RQSpline(nn.Module):
             layers.append(distrax.MaskedCoupling(
                 mask=mask_all, bijector=scalar_affine, conditioner=self.scalar[i]))
             layers.append(distrax.MaskedCoupling(
-                mask=mask, bijector=bijector_fn, conditioner=self.conditioner[i]))
+                mask=mask, bijector=self.bijector_fn, conditioner=self.conditioner[i]))
             mask = jnp.logical_not(mask)
 
         flow = distrax.Inverse(distrax.Chain(layers))

src/flowMC/sampler/Sampler.py
@@ -39,7 +39,8 @@ class Sampler(object):
                 stepsize: float = 1e-3,
                 use_global: bool = True,
                 logging: bool = True,
-                nf_variable = None):
+                nf_variable = None,
+                keep_quantile=0):
         rng_key_init ,rng_keys_mcmc, rng_keys_nf, init_rng_keys_nf = rng_key_set
 
         self.local_sampler = local_sampler
@@ -71,6 +72,7 @@ class Sampler(object):
         if nf_variable is not None:
             self.variables = self.variables
 
+        self.keep_quantile = keep_quantile
         self.nf_training_loop, train_epoch, train_step = make_training_loop(self.nf_model) 
         self.global_sampler = make_nf_metropolis_sampler(self.nf_model)
 
@@ -137,16 +139,24 @@ class Sampler(object):
 
             
         if self.use_global == True:
-            chain_size = positions.shape[0]*positions.shape[1]
-            if chain_size > self.max_samples:
-                flat_chain = positions[:,-int(self.max_samples/self.n_chains):].reshape(-1,self.n_dim)
+            if self.keep_quantile > 0:
+                max_log_prob = jnp.max(log_prob_output, axis=1)
+                cut = jnp.quantile(max_log_prob, self.keep_quantile)
+                cut_chains = positions[max_log_prob > cut]
             else:
-                flat_chain = positions.reshape(-1, self.n_dim)
+                cut_chains = positions
+            chain_size = cut_chains.shape[0]*cut_chains.shape[1]
+            if chain_size > self.max_samples:
+                flat_chain = cut_chains[:,-int(self.max_samples/self.n_chains):].reshape(-1,self.n_dim)
+            else:
+                flat_chain = cut_chains.reshape(-1, self.n_dim)
 
             variables = self.variables.unfreeze()
             variables['base_mean'] = jnp.mean(flat_chain, axis=0)
             variables['base_cov'] = jnp.cov(flat_chain.T)
             self.variables = flax.core.freeze(variables)
+
+            flat_chain = (flat_chain-variables['base_mean'])/jnp.sqrt(jnp.diag(variables['base_cov']))
 
             rng_keys_nf, state, loss_values = self.nf_training_loop(rng_keys_nf, state, self.variables, flat_chain,
                                                                     self.n_epochs, self.batch_size)