711a2a6f7fbde90cdc1bd049a49bf65cccc83f4a
Kaze Wong <kazewong.physics@gmail.com>
2022-09-23 23:16:19 -0400

    Update readthedocs.yml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,21 +39,7 @@ extensions = [
     "myst_parser"
 ]
 
-myst_enable_extensions = [
-    "amsmath",
-    "colon_fence",
-    "deflist",
-    "dollarmath",
-    "fieldlist",
-    "html_admonition",
-    "html_image",
-    "linkify",
-    "replacements",
-    "smartquotes",
-    "strikethrough",
-    "substitution",
-    "tasklist",
-]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
