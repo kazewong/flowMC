5e50e66fbcb55d803100bcb92ad71caf714c26c8
Kaze Wong <kazewong.physics@gmail.com>
2022-09-23 23:02:00 -0400

    Change the theme to sphnix book theme, looks so good

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ copyright = "2022, Kaze Wong, Marylou Gabrié"
 author = "Kaze Wong, Marylou Gabrié"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.2"
+release = "0.0.4"
 master_doc = "index"
 
 # -- General configuration ---------------------------------------------------
@@ -36,6 +36,23 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosummary",
+    "myst_parser"
+]
+
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -52,7 +69,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_book_theme"
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
