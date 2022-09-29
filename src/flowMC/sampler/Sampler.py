042d9d4ee333209c313a9f42084fed24a46dd498
Kaze Wong <kazewong.physics@gmail.com>
2022-09-29 10:46:01 -0400

    minor update

--- src/flowMC/sampler/Sampler.py
+++ src/flowMC/sampler/Sampler.py
@@ -188,7 +188,6 @@ class Sampler():
                 variables["base_cov"] = jnp.cov(flat_chain.T)
                 self.variables = flax.core.freeze(variables)
 
-
                 self.rng_keys_nf, self.state, loss_values = self.nf_training_loop(
                     self.rng_keys_nf,
                     self.state,
