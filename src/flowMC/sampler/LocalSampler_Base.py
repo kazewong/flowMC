e8b988f530e010c2700ba6031fd2b81f9df5e52e
kazewong <kazewong.physics@gmail.com>
2022-11-04 04:42:40 -0400

    Update LocalSamplerBase class

--- src/flowMC/sampler/LocalSampler_Base.py
+++ src/flowMC/sampler/LocalSampler_Base.py
@@ -1,1 +1,25 @@
+from abc import abstractmethod
+from typing import Callable
+class LocalSamplerBase:
 
+    @abstractmethod
+    def __init__(self, logpdf: Callable, jit: bool, params: dict) -> Callable:
+        """
+        
+        """
+
+    @abstractmethod
+    def make_kernel(self, return_aux = False) -> Callable:
+        """
+        
+        """
+
+    @abstractmethod
+    def make_update(self) -> Callable:
+        """
+        """
+
+    @abstractmethod
+    def make_sampler(self) -> Callable:
+        """
+        """
