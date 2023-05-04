0579de28567f23be4648f5c98dcfb6cf16a60ae3
Kaze Wong <kazewong.physics@gmail.com>
2023-05-04 13:29:28 -0400

    Update test_MALA

--- test/integration/test_MALA.py
+++ test/integration/test_MALA.py
@@ -3,13 +3,14 @@ from flowMC.utils.PRNG_keys import initialize_rng_keys
 import jax
 import jax.numpy as jnp
 from jax.scipy.special import logsumexp
+import numpy as np
 
-def dual_moon_pe(x, data=None):
+def dual_moon_pe(x, data):
     """
     Term 2 and 3 separate the distribution and smear it along the first and second dimension
     """
     print("compile count")
-    term1 = 0.5 * ((jnp.linalg.norm(x) - 2) / 0.1) ** 2
+    term1 = 0.5 * ((jnp.linalg.norm(x-data) - 2) / 0.1) ** 2
     term2 = -0.5 * ((x[:1] + jnp.array([-3.0, 3.0])) / 0.8) ** 2
     term3 = -0.5 * ((x[1:2] + jnp.array([-3.0, 3.0])) / 0.6) ** 2
     return -(term1 - logsumexp(term2) - logsumexp(term3))
@@ -20,26 +21,28 @@ n_local_steps = 30
 step_size = 0.01
 n_leapfrog = 10
 
+data = jnp.array(np.random.normal(5))
+
 rng_key_set = initialize_rng_keys(n_chains, seed=42)
 
 initial_position = jax.random.normal(rng_key_set[0], shape=(n_chains, n_dim)) * 1
 
 MALA_Sampler = MALA(dual_moon_pe, True, {"step_size": step_size})
 
-MALA_Sampler.precompilation(n_chains, n_dim, n_local_steps, None)
+MALA_Sampler.precompilation(n_chains, n_dim, n_local_steps, data)
 
 
 initial_position = jnp.repeat(initial_position[:,None], n_local_steps, 1)
-initial_logp = jnp.repeat(jax.vmap(dual_moon_pe)(initial_position[:,0])[:,None], n_local_steps, 1)[...,None]
+initial_logp = jnp.repeat(jax.vmap(dual_moon_pe, in_axes=(0,None))(initial_position[:,0], data)[:,None], n_local_steps, 1)[...,None]
 
-state = (rng_key_set[1], initial_position, initial_logp, jnp.zeros((n_chains, n_local_steps,1)), None, MALA_Sampler.params)
+state = (rng_key_set[1], initial_position, initial_logp, jnp.zeros((n_chains, n_local_steps,1)), data, MALA_Sampler.params)
 
 
 MALA_Sampler.update_vmap(1, state)
 
 MALA_Sampler_sampler = MALA_Sampler.make_sampler()
 
-state = MALA_Sampler_sampler(rng_key_set[1], n_local_steps, initial_position[:,0], None)
+state = MALA_Sampler_sampler(rng_key_set[1], n_local_steps, initial_position[:,0], data)
 
 
 from flowMC.nfmodel.rqSpline import RQSpline
@@ -64,7 +67,7 @@ print("Initializing sampler class")
 nf_sampler = Sampler(
     n_dim,
     rng_key_set,
-    None,
+    jnp.arange(5),
     MALA_Sampler,
     model,
     n_loop_training=n_loop_training,
@@ -76,5 +79,5 @@ nf_sampler = Sampler(
     use_global=False,
 )
 
-nf_sampler.sample(initial_position, None)
+nf_sampler.sample(initial_position, data)
 
