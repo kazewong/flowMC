8098ff57ec48da876da31616df8bf29de8a11795
Kaze Wong <kazewong.physics@gmail.com>
2023-05-05 13:38:17 -0400

    HMC now runs, tho the compile count is actually pretty high. Need to optimize a bit in the future

--- test/integration/test_HMC.py
+++ test/integration/test_HMC.py
@@ -4,14 +4,16 @@ import jax
 import jax.numpy as jnp
 from jax.scipy.special import logsumexp
 
-def dual_moon_pe(x):
+def dual_moon_pe(x, data):
     """
     Term 2 and 3 separate the distribution and smear it along the first and second dimension
     """
-    term1 = 0.5 * ((jnp.linalg.norm(x) - 2) / 0.1) ** 2
+    print("compile count")
+    term1 = 0.5 * ((jnp.linalg.norm(x-data) - 2) / 0.1) ** 2
     term2 = -0.5 * ((x[:1] + jnp.array([-3.0, 3.0])) / 0.8) ** 2
     term3 = -0.5 * ((x[1:2] + jnp.array([-3.0, 3.0])) / 0.6) ** 2
     return -(term1 - logsumexp(term2) - logsumexp(term3))
+
 
 n_dim = 5
 n_chains = 15
@@ -19,32 +21,27 @@ n_local_steps = 30
 step_size = 0.1
 n_leapfrog = 10
 
+data = jnp.arange(5)
+
 rng_key_set = initialize_rng_keys(n_chains, seed=42)
 
 initial_position = jax.random.normal(rng_key_set[0], shape=(n_chains, n_dim)) * 1
 
 HMC = HMC(dual_moon_pe, True, {"step_size": step_size,"n_leapfrog": n_leapfrog, "inverse_metric": jnp.ones(n_dim)})
 
-initial_PE = jax.vmap(HMC.potential)(initial_position)
+initial_PE = HMC.logpdf_vmap(initial_position, data)
 
-HMC_kernel = HMC.make_kernel()
-
-print(HMC_kernel(rng_key_set[0], initial_position[0], initial_PE[0], HMC.params))
-
-HMC_update = HMC.make_update()
-HMC_update = jax.vmap(HMC_update, in_axes = (None, (0, 0, 0, 0, None)), out_axes=(0, 0, 0, 0, None))
+HMC.precompilation(n_chains, n_dim, n_local_steps, data)
 
 initial_position = jnp.repeat(initial_position[:,None], n_local_steps, 1)
 initial_PE = jnp.repeat(initial_PE[:,None], n_local_steps, 1)
 
-state = (rng_key_set[1], initial_position, initial_PE, jnp.zeros((n_chains, n_local_steps,1)), HMC.params)
+state = (rng_key_set[1], initial_position, initial_PE, jnp.zeros((n_chains, n_local_steps,1)), data, HMC.params)
 
-
-HMC_update(1, state)
-
+HMC.update_vmap(1, state)
 HMC_sampler = HMC.make_sampler()
 
-state = HMC_sampler(rng_key_set[1], n_local_steps, initial_position[:, 0])
+state = HMC_sampler(rng_key_set[1], n_local_steps, initial_position[:, 0], data)
 
 
 from flowMC.nfmodel.rqSpline import RQSpline
@@ -69,9 +66,9 @@ print("Initializing sampler class")
 nf_sampler = Sampler(
     n_dim,
     rng_key_set,
+    jnp.arange(5),
     HMC,
-    dual_moon_pe,
-    model   ,
+    model,
     n_loop_training=n_loop_training,
     n_loop_production=n_loop_production,
     n_local_steps=n_local_steps,
@@ -80,4 +77,4 @@ nf_sampler = Sampler(
     use_global=False,
 )
 
-nf_sampler.sample(initial_position)
+nf_sampler.sample(initial_position, data)
