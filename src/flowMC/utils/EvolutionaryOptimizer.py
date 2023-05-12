4116362dcc673d87b383a76d337db890a5d90d6b
Kaze Wong <kazewong.physics@gmail.com>
2023-05-12 10:00:34 -0400

    Add EvoluitonaryOptimizer class

---
+++ src/flowMC/utils/EvolutionaryOptimizer.py
@@ -0,0 +1,34 @@
+from evosax import CMA_ES
+import jax
+import jax.numpy as jnp
+import tqdm
+
+class EvolutionaryOptimizer:
+
+    """
+    A wrapper class for the evosax package.
+    Note that we do not aim to solve any generic optimization problem,
+    especially in a high dimension space.
+    """
+
+    def __init__(self, ndims, popsize=100, verbose=False):
+        self.strategy = CMA_ES(num_dims=ndims, popsize=popsize, elite_ratio=0.5)
+        self.es_params = self.strategy.default_params.replace(clip_min=0, clip_max=1)
+        self.verbose = verbose
+
+    def optimize(self, objective, bound, n_loops = 100, seed = 9527):
+        rng = jax.random.PRNGKey(seed)
+        progress_bar = tqdm.tqdm(range(n_loops), "Generation: ") if self.verbose else range(n_loops)
+        self.state = self.strategy.initialize(rng, self.es_params)
+        for i in progress_bar:
+            rng, rng_gen, rng_eval = jax.random.split(rng, 3)
+            x, state = self.strategy.ask(rng_gen, self.state, self.es_params)
+            theta = x * (bound[:, 1] - bound[:, 0]) + bound[:, 0]
+            fitness = objective(theta)
+            self.state = self.strategy.tell(x, fitness.astype(jnp.float32), state, self.es_params)
+            if self.verbose: progress_bar.set_description(f"Generation: {i}, Fitness: {fitness.mean():.4f}")
+
+        return self.state
+
+    def get_result(self):
+        return self.state
