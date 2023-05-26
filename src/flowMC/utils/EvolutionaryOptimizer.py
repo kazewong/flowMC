c84a9e726765ac579fcd9f8cae5f190d87b26577
kazewong <kazewong.physics@gmail.com>
2023-05-26 11:24:25 -0400

    Add documentation to the EvolutionaryOptimizer

--- src/flowMC/utils/EvolutionaryOptimizer.py
+++ src/flowMC/utils/EvolutionaryOptimizer.py
@@ -9,6 +9,32 @@ class EvolutionaryOptimizer:
     A wrapper class for the evosax package.
     Note that we do not aim to solve any generic optimization problem,
     especially in a high dimension space.
+
+
+    Parameters
+    ----------
+    ndims : int
+        The dimension of the parameter space.
+    popsize : int
+        The population size of the evolutionary algorithm.
+    verbose : bool
+        Whether to print the progress bar.
+
+    Attributes
+    ----------
+    strategy : evosax.CMA_ES
+        The evolutionary strategy.
+    es_params : evosax.CMA_ESParams
+        The parameters of the evolutionary strategy.
+    verbose : bool
+        Whether to print the progress bar.
+
+    Methods
+    -------
+    optimize(objective, bound, n_loops = 100, seed = 9527)
+        Optimize the objective function.
+    get_result()
+        Get the best member and the best fitness.
     """
 
     def __init__(self, ndims, popsize=100, verbose=False):
@@ -17,6 +43,24 @@ class EvolutionaryOptimizer:
         self.verbose = verbose
 
     def optimize(self, objective, bound, n_loops = 100, seed = 9527):
+        """
+        Optimize the objective function.
+
+        Parameters
+        ----------
+        objective : Callable
+            The objective function, which should be implemented in JAX.
+        bound : (2, ndims) ndarray
+            The bound of the parameter space.
+        n_loops : int
+            The number of iterations.
+        seed : int
+            The random seed.
+
+        Returns
+        -------
+        None
+        """
         rng = jax.random.PRNGKey(seed)
         progress_bar = tqdm.tqdm(range(n_loops), "Generation: ") if self.verbose else range(n_loops)
         self.bound = bound
