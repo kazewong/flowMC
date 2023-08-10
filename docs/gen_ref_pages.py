88769b271071c5362a49eccce34d2f36661f296f
Kaze Wong <kazewong.physics@gmail.com>
2023-08-10 10:28:13 -0400

    Autodoc working

--- docs/gen_ref_pages.py
+++ docs/gen_ref_pages.py
@@ -10,7 +10,7 @@ nav = mkdocs_gen_files.Nav()
 for path in sorted(Path("src").rglob("*.py")):  # 
     module_path = path.relative_to("src").with_suffix("")  # 
     doc_path = path.relative_to("src").with_suffix(".md")  # 
-    full_doc_path = Path("reference", doc_path)  # 
+    full_doc_path = Path("api", doc_path)  # 
 
     parts = list(module_path.parts)
 
@@ -21,11 +21,13 @@ for path in sorted(Path("src").rglob("*.py")):  #
     
     nav[parts] = doc_path.as_posix()
 
+    print(full_doc_path)
+
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:  # 
         identifier = ".".join(parts)  # 
         print("::: " + identifier, file=fd)  # 
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path)  # 
 
-    with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:  # 
+    with mkdocs_gen_files.open("api/summary.md", "w") as nav_file:  # 
         nav_file.writelines(nav.build_literate_nav())
