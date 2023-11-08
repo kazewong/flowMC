Correct hamiltonian sign error

src/flowMC/sampler/Sampler.py
@@ -248,7 +248,8 @@ class Sampler:
                 self.n_global_steps,
                 positions[:, -1],
                 data,
-                verbose = self.verbose
+                verbose = self.verbose,
+                mode = summary_mode
             )
 
             self.summary[summary_mode]["chains"] = jnp.append(

src/flowMC/sampler/flowHMC.py
@@ -57,7 +57,7 @@ class flowHMC(HMC, NFProposal):
 
         key1, key2 = jax.random.split(rng_key)
 
-        momentum = jax.random.normal(key1, shape=position.shape) @ flow_cov
+        momentum = jnp.dot(jax.random.normal(key1, shape=position.shape), jnp.linalg.cholesky(flow_cov).T)
         mass_matrix = jnp.linalg.inv(flow_cov)
 
         # TODO: Double check whether I can compute the hamiltonian before the map
@@ -68,7 +68,7 @@ class flowHMC(HMC, NFProposal):
         middle_position, middle_momentum = self.leapfrog_step(
             position, momentum, data, mass_matrix
         )
-    
+
         # Push through map
 
         flow_start_prob = self.model.log_prob(middle_position[None])
@@ -84,7 +84,7 @@ class flowHMC(HMC, NFProposal):
 
         # Compute acceptance probability
 
-        log_acc = (final_Ham - initial_Ham) - (flow_end_prob - flow_start_prob)
+        log_acc = -(final_Ham - initial_Ham) - (flow_end_prob - flow_start_prob)
 
         uniform_random = jnp.log(jax.random.uniform(key2))
         do_accept = log_acc > uniform_random
@@ -126,6 +126,7 @@ class flowHMC(HMC, NFProposal):
         initial_position: Float[Array, "n_chains ndim"],
         data: PyTree,
         verbose: bool = False,
+        mode: str = "training",
     ) -> tuple[
         Float[Array, "n_chains n_steps ndim"],
         Float[Array, "n_chains n_steps 1"],

test/integration/test_flowHMC.py
@@ -8,18 +8,18 @@ from flowMC.nfmodel.rqSpline import MaskedCouplingRQSpline
 from flowMC.sampler.Sampler import Sampler
 
 
-# def dual_moon_pe(x, data):
-#     """
-#     Term 2 and 3 separate the distribution and smear it along the first and second dimension
-#     """
-#     print("compile count")
-#     term1 = 0.5 * ((jnp.linalg.norm(x - data) - 2) / 0.1) ** 2
-#     term2 = -0.5 * ((x[:1] + jnp.array([-3.0, 3.0])) / 0.8) ** 2
-#     term3 = -0.5 * ((x[1:2] + jnp.array([-3.0, 3.0])) / 0.6) ** 2
-#     return -(term1 - logsumexp(term2) - logsumexp(term3))
-
 def log_posterior(x, data):
-    return -0.5 * jnp.sum((x-data) ** 2)
+    """
+    Term 2 and 3 separate the distribution and smear it along the first and second dimension
+    """
+    print("compile count")
+    term1 = 0.5 * ((jnp.linalg.norm(x - data) - 2) / 0.1) ** 2
+    term2 = -0.5 * ((x[:1] + jnp.array([-3.0, 3.0])) / 0.8) ** 2
+    term3 = -0.5 * ((x[1:2] + jnp.array([-3.0, 3.0])) / 0.6) ** 2
+    return -(term1 - logsumexp(term2) - logsumexp(term3))
+
+# def log_posterior(x, data):
+#     return -0.5 * jnp.sum((x-data) ** 2)
 
 n_dim = 2
 n_chains = 15
@@ -74,7 +74,7 @@ nf_sampler = Sampler(n_dim,
                     n_epochs = 30,
                     learning_rate = 1e-2,
                     batch_size = 1000,
-                    n_chains = n_chains,)
+                    n_chains = n_chains)#,
                     # global_sampler = flowHMC_sampler)
 
 nf_sampler.sample(initial_position, data)