ebbd4137b53a0a8db734f15734726e21c388ea5c
Kaze Wong <kazewong.physics@gmail.com>
2024-03-26 14:34:14 -0400

    Nuke PRNG_key

--- src/flowMC/utils/PRNG_keys.py
+++ src/flowMC/utils/PRNG_keys.py
@@ -1,25 +1,25 @@
 import jax
 
 
-def initialize_rng_keys(n_chains, seed=42):
-    """
-    Initialize the random number generator keys for the sampler.
+# def initialize_rng_keys(n_chains, seed=42):
+#     """
+#     Initialize the random number generator keys for the sampler.
 
-    Args:
-        n_chains (int): Number of chains for the local sampler.
-        seed (int): Seed for the random number generator.
+#     Args:
+#         n_chains (int): Number of chains for the local sampler.
+#         seed (int): Seed for the random number generator.
 
 
-    Returns:
-        rng_keys_init (Device Array): RNG keys for sampling initial position from prior.
-        rng_keys_mcmc (Device Array): RNG keys for the local sampler.
-        rng_keys_nf (Device Array): RNG keys for the normalizing flow global sampler.
-        init_rng_keys_nf (Device Array): RNG keys for initializing wieght of the normalizing flow model.
-    """
-    rng_key = jax.random.PRNGKey(seed)
-    rng_key_init, rng_key_mcmc, rng_key_nf = jax.random.split(rng_key, 3)
+#     Returns:
+#         rng_keys_init (Device Array): RNG keys for sampling initial position from prior.
+#         rng_keys_mcmc (Device Array): RNG keys for the local sampler.
+#         rng_keys_nf (Device Array): RNG keys for the normalizing flow global sampler.
+#         init_rng_keys_nf (Device Array): RNG keys for initializing wieght of the normalizing flow model.
+#     """
+#     rng_key = jax.random.PRNGKey(seed)
+#     rng_key_init, rng_key_mcmc, rng_key_nf = jax.random.split(rng_key, 3)
 
-    rng_keys_mcmc = jax.random.split(rng_key_mcmc, n_chains)
-    rng_keys_nf, init_rng_keys_nf = jax.random.split(rng_key_nf, 2)
+#     rng_keys_mcmc = jax.random.split(rng_key_mcmc, n_chains)
+#     rng_keys_nf, init_rng_keys_nf = jax.random.split(rng_key_nf, 2)
 
-    return rng_key_init, rng_keys_mcmc, rng_keys_nf, init_rng_keys_nf
+#     return rng_key_init, rng_keys_mcmc, rng_keys_nf, init_rng_keys_nf
