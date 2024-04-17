6665bc2c5187e68b5e8066098a94be91c0634a82
Kaze Wong <kazewong.physics@gmail.com>
2024-04-17 17:06:06 -0400

    Add standalone optimization function into optimization_Adam

--- src/flowMC/strategy/optimization.py
+++ src/flowMC/strategy/optimization.py
@@ -10,11 +10,17 @@ from flowMC.strategy.base import Strategy
 
 
 class optimization_Adam(Strategy):
-
     """
     Optimize a set of chains using Adam optimization.
     Note that if the posterior can go to infinity, this optimization scheme is likely to return NaNs.
-    
+
+    Args:
+        n_steps: int = 100
+            Number of optimization steps.
+        learning_rate: float = 1e-2
+            Learning rate for the optimization.
+        noise_level: float = 10
+            Variance of the noise added to the gradients.
     """
 
     n_steps: int = 100
@@ -24,7 +30,7 @@ class optimization_Adam(Strategy):
     @property
     def __name__(self):
         return "AdamOptimization"
-    
+
     def __init__(
         self,
         **kwargs,
@@ -91,11 +97,77 @@ class optimization_Adam(Strategy):
         summary["final_positions"] = optimized_positions
         summary["final_log_prob"] = local_sampler.logpdf_vmap(optimized_positions, data)
 
-        if jnp.isinf(summary['final_log_prob']).any() or jnp.isnan(summary['final_log_prob']).any():
+        if (
+            jnp.isinf(summary["final_log_prob"]).any()
+            or jnp.isnan(summary["final_log_prob"]).any()
+        ):
             print("Warning: Optimization accessed infinite or NaN log-probabilities.")
-            
+
         return rng_key, optimized_positions, local_sampler, global_sampler, summary
 
+    def optimize(
+        self,
+        rng_key: PRNGKeyArray,
+        objective: Callable,
+        initial_position: Float[Array, " n_chain n_dim"],
+    ):
+        """
+        Standalone optimization function that takes an objective function and returns the optimized positions.
+
+        Args:
+            rng_key: PRNGKeyArray
+                Random key for the optimization.
+            objective: Callable
+                Objective function to optimize.
+            initial_position: Float[Array, " n_chain n_dim"]
+                Initial positions for the optimization.
+        """
+        grad_fn = jax.jit(jax.grad(objective))
+
+        def _kernel(carry, data):
+            key, params, opt_state = carry
+
+            key, subkey = jax.random.split(key)
+            grad = grad_fn(params) * (1 + jax.random.normal(subkey) * self.noise_level)
+            updates, opt_state = self.solver.update(grad, opt_state, params)
+            params = optax.apply_updates(params, updates)
+            return (key, params, opt_state), None
+
+        def _single_optimize(
+            key: PRNGKeyArray,
+            initial_position: Float[Array, " n_dim"],
+        ) -> Float[Array, " n_dim"]:
+
+            opt_state = self.solver.init(initial_position)
+
+            (key, params, opt_state), _ = jax.lax.scan(
+                _kernel,
+                (key, initial_position, opt_state),
+                jnp.arange(self.n_steps),
+            )
+
+            return params  # type: ignore
+
+        print("Using Adam optimization")
+        rng_key, subkey = jax.random.split(rng_key)
+        keys = jax.random.split(subkey, initial_position.shape[0])
+        optimized_positions = jax.vmap(_single_optimize, in_axes=(0, 0))(
+            keys, initial_position
+        )
+
+        summary = {}
+        summary["initial_positions"] = initial_position
+        summary["initial_log_prob"] = jax.jit(jax.vmap(objective))(initial_position)
+        summary["final_positions"] = optimized_positions
+        summary["final_log_prob"] = jax.jit(jax.vmap(objective))(optimized_positions)
+
+        if (
+            jnp.isinf(summary["final_log_prob"]).any()
+            or jnp.isnan(summary["final_log_prob"]).any()
+        ):
+            print("Warning: Optimization accessed infinite or NaN log-probabilities.")
+
+        return rng_key, optimized_positions, summary
 
 class Evosax_CMA_ES(Strategy):
 
