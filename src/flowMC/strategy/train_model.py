595e9c217816d5152909aef299258d07d2c381fa
Kaze Wong <kazewong.physics@gmail.com>
2024-12-31 13:03:51 -0500

    update train_model

--- src/flowMC/strategy/train_model.py
+++ src/flowMC/strategy/train_model.py
@@ -3,16 +3,16 @@ from flowMC.resource.base import Resource
 from flowMC.resource.buffers import Buffer
 from flowMC.resource.nf_model.base import NFModel
 from flowMC.resource.optimizer import Optimizer
-from jaxtyping import Array, Float, PRNGKeyArray, PyTree
-import optax
-import equinox as eqx
+from jaxtyping import Array, Float, PRNGKeyArray
 import jax
 import jax.numpy as jnp
+
 
 class TrainModel(Strategy):
 
     model_resource: str
     data_resource: str
+    optimizer_resource: str
     n_epochs: int
     batch_size: int
     n_max_examples: int
@@ -22,15 +22,17 @@ class TrainModel(Strategy):
     def __str__(self):
         return "Train " + self.model_resource
 
-    def __init__(self,
-                 target_resource: str,
-                 data_resource: str,
-                 optimizer_resource: str,
-                 n_epochs: int = 100,
-                 batch_size: int = 64,
-                 n_max_examples: int = 10000,
-                 verbose: bool = False,
-                 thinning: int = 1,):
+    def __init__(
+        self,
+        target_resource: str,
+        data_resource: str,
+        optimizer_resource: str,
+        n_epochs: int = 100,
+        batch_size: int = 64,
+        n_max_examples: int = 10000,
+        thinning: int = 1,
+        verbose: bool = False,
+    ):
         self.target_resource = target_resource
         self.data_resource = data_resource
         self.optimizer_resource = optimizer_resource
@@ -49,15 +51,17 @@ class TrainModel(Strategy):
         data: dict,
     ) -> tuple[
         PRNGKeyArray,
-        dict[str,Resource],
+        dict[str, Resource],
         Float[Array, "n_chains n_dim"],
     ]:
         model = resources[self.target_resource]
         assert isinstance(model, NFModel), "Target resource must be a NFModel"
-        data_resource  = resources[self.data_resource]
+        data_resource = resources[self.data_resource]
         assert isinstance(data_resource, Buffer), "Data resource must be a buffer"
         optimizer = resources[self.optimizer_resource]
-        assert isinstance(optimizer, Optimizer), "Optimizer resource must be an optimizer"
+        assert isinstance(
+            optimizer, Optimizer
+        ), "Optimizer resource must be an optimizer"
         training_data = data_resource.buffer
         rng_key, subkey = jax.random.split(rng_key)
         (rng_key, model, optim_state, loss_values) = model.train(
