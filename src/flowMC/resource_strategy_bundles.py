79c91f1e95e2d62654612c3c519b44849ca6fa3a
Kaze Wong <kazewong.physics@gmail.com>
2025-02-04 10:42:44 -0500

    Make resource strategy bundles

--- src/flowMC/resource_strategy_bundles.py
+++ src/flowMC/resource_strategy_bundles.py
@@ -1,25 +1,42 @@
+from abc import ABC, abstractmethod
+from typing import Callable
+
+import jax
 from jaxtyping import Array, Float, PRNGKeyArray, PyTree
 from tqdm import tqdm
 
-from flowMC.strategy.base import Strategy
 from flowMC.resource.base import Resource
+from flowMC.resource.buffers import Buffer
 from flowMC.resource.local_kernel.base import ProposalBase
-from flowMC.strategy.take_steps import TakeSerialSteps, TakeGroupSteps
-from flowMC.strategy.train_model import TrainModel
-from typing import Callable
+from flowMC.resource.local_kernel.MALA import MALA
+from flowMC.resource.nf_model.NF_proposal import NFProposal
+from flowMC.resource.nf_model.rqSpline import MaskedCouplingRQSpline
+from flowMC.resource.optimizer import Optimizer
+from flowMC.strategy.base import Strategy
+from flowMC.strategy.global_tuning import LocalGlobalNFSample
 
-class GlobalTuning(Strategy):
 
-    logpdf: Callable[[Float[Array, " n_dim"], dict], Float]
-    local_stepper: TakeSerialSteps
-    global_stepper: TakeGroupSteps
-    training_stepper: TrainModel
+class ResourceStrategyBundle(ABC):
+    """
+    Resource-Strategy Bundle is aim to be the highest level of abstraction in the flowMC library.
+    It is a collection of resources and strategies that are used to perform a specific task.
+    """
+
+    resources: dict[str, Resource]
+    strategies: list[Strategy]
+
+
+class LocalGlobalNFSamplingBundle(ResourceStrategyBundle):
 
     def __str__(self):
-        return "GlobalTuning"
+        return "Local Global NF Sampling"
 
     def __init__(
         self,
+        rng_key: PRNGKeyArray,
+        n_chains: int,
+        n_steps: int,
+        n_dim: int,
         logpdf: Callable[[Float[Array, " n_dim"], dict], Float],
         local_sampler: ProposalBase,
         global_sampler: ProposalBase,
@@ -28,65 +45,42 @@ class GlobalTuning(Strategy):
         global_names: list[str],
         n_local_steps: int,
         n_global_steps: int,
-        n_loop: int,
+        n_training_loops: int,
+        n_production_loops: int,
         n_epochs: int,
     ):
-        self.logpdf = logpdf
-        self.local_stepper = TakeSerialSteps(
-            logpdf,
-            local_sampler,
-            local_names,
-            n_local_steps,
-        )
-        self.global_stepper = TakeGroupSteps(
-            logpdf,
-            global_sampler,
-            global_names,
-            n_global_steps,
-        )
-        self.training_stepper = TrainModel(
-            training_names[0],
-            training_names[1],
-            training_names[2],
-            n_epochs,
-        )
-        self.n_loop = n_loop
 
-    def __call__(
-        self,
-        rng_key: PRNGKeyArray,
-        resources: dict[str, Resource],
-        initial_position: Float[Array, "n_chains n_dim"],
-        data: dict,
-    ) -> tuple[
-        PRNGKeyArray,
-        dict[str, Resource],
-        Float[Array, "n_chains n_dim"],
-    ]:
-        for _ in tqdm(
-            range(self.n_loop),
-            desc="Global Tuning",
-        ):
-            rng_key, resources, initial_position = self.local_stepper(
-                rng_key,
-                resources,
-                initial_position,
-                data,
-            )
-            self.global_stepper.set_current_position(self.local_stepper.current_position)
-            rng_key, resources, initial_position = self.training_stepper(
-                rng_key,
-                resources,
-                initial_position,
-                data,
-            )
-            rng_key, resources, initial_position = self.global_stepper(
-                rng_key,
-                resources,
-                initial_position,
-                data,
-            )
-            # TODO: This is current wasteful since only half of local/global acceptance are populated
-            self.local_stepper.set_current_position(self.global_stepper.current_position)
+        positions = Buffer("positions", n_chains, n_steps, n_dim)
+        log_prob = Buffer("log_prob", n_chains, n_steps, 1)
+        local_accs = Buffer("local_accs", n_chains, n_steps, 1)
+        global_accs = Buffer("global_accs", n_chains, n_steps, 1)
+        local_sampler = MALA(step_size=1e-1)
+        rng_key, subkey = jax.random.split(rng_key)
+        model = MaskedCouplingRQSpline(n_dim, 3, [64, 64], 8, subkey)
+        global_sampler = NFProposal(model)
+        optimizer = Optimizer(model=model)
 
-        return rng_key, resources, initial_position
+        self.resources = {
+            "positions": positions,
+            "log_prob": log_prob,
+            "local_accs": local_accs,
+            "global_accs": global_accs,
+            "model": model,
+            "optimizer": optimizer,
+        }
+
+        self.strategies = [
+            LocalGlobalNFSample(
+                logpdf,
+                local_sampler,
+                global_sampler,
+                local_names,
+                training_names,
+                global_names,
+                n_local_steps,
+                n_global_steps,
+                n_training_loops,
+                n_production_loops,
+                n_epochs,
+            )
+        ]
