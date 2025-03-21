d4a49457af1672fa2eeb32361dfd54ddb443fe3e
Kaze Wong <kazewong.physics@gmail.com>
2025-03-21 14:49:14 -0400

    Add LogPDF integration to HMC, MALA, and RWMCMC tests

--- test/integration/test_HMC.py
+++ test/integration/test_HMC.py
@@ -6,6 +6,7 @@ from jaxtyping import Array, Float
 from flowMC.resource.local_kernel.HMC import HMC
 from flowMC.strategy.take_steps import TakeSerialSteps
 from flowMC.resource.buffers import Buffer
+from flowMC.resource.logPDF import LogPDF
 from flowMC.Sampler import Sampler
 
 
@@ -51,11 +52,12 @@ resource = {
     "log_prob": log_prob,
     "acceptance": acceptance,
     "HMC": HMC_sampler,
+    "logpdf": LogPDF(dual_moon_pe, n_dims=n_dims),
 }
 
 # Define strategy
 strategy = TakeSerialSteps(
-    logpdf=dual_moon_pe,
+    "logpdf",
     kernel_name="HMC",
     buffer_names=["positions", "log_prob", "acceptance"],
     n_steps=n_local_steps,
