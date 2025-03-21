4eb370f3d6fa11f9a25b061ccfc1927056fb21c9
Kaze Wong <kazewong.physics@gmail.com>
2025-03-21 14:21:12 -0400

    Add LogPDF class

--- src/flowMC/resource/logPDF.py
+++ src/flowMC/resource/logPDF.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Callable
+from typing import Callable, Optional
 from flowMC.resource.base import Resource
 from jaxtyping import Array, Float, PyTree
 import jax.numpy as jnp
@@ -13,34 +13,47 @@ class Variable:
 @jax.tree_util.register_pytree_node_class
 class LogPDF(Resource):
 
-    """
+    """LogPDF class 
     
     """
-
     log_pdf: Callable[[Float[Array, " n_dim"], PyTree], Float[Array, "1"]]
     variables: list[Variable]
-    data: PyTree
 
     @property
-    def n_dim(self):
+    def n_dims(self):
         return len(self.variables)
 
     def __repr__(self):
         return super().__repr__()
     
-    def __init__(self, log_pdf: Callable[[Float[Array, " n_dim"], PyTree], Float[Array, "1"]], variables: PyTree, data: PyTree):
+    def __init__(self, log_pdf: Callable[[Float[Array, " n_dim"], PyTree], Float[Array, "1"]], variables: Optional[list[Variable]] = None, n_dims: Optional[int] = None):
         self.log_pdf = log_pdf
-        self.variables = variables
-        self.data = data
+        if variables is None and n_dims is not None:
+            self.variables = [Variable("x_"+str(i), True) for i in range(n_dims)]
+        elif variables is not None:
+            self.variables = variables
+        else:
+            raise ValueError("Either variables or n_dims must be provided")
 
-    def __call__(self, x: Float[Array, " n_dim"]) -> Float[Array, "1"]:
-        return self.log_pdf(x, self.data)
+    def __call__(self, x: Float[Array, " n_dim"], data: PyTree) -> Float[Array, "1"]:
+        return self.log_pdf(x, data)
+
+    def print_parameters(self):
+        print("LogPDF with variables:")
+        for var in self.variables:
+            print(var.name, var.continuous)
+    
+    def save_resource(self, path):
+        raise NotImplementedError
+    
+    def load_resource(self, path):
+        raise NotImplementedError
 
     def tree_flatten(self):
-        children = (self.log_pdf, self.variables, self.data)
-        aux_data = {}
+        children = ()
+        aux_data = (self.log_pdf, self.variables)
         return (children, aux_data)
     
     @classmethod
     def tree_unflatten(cls, aux_data, children):
-        return cls(*children)
+        return cls(aux_data[0], aux_data[1])
