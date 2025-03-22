88936ade24f0591b22e98243c50b8f3ebb534e0c
Kaze Wong <kazewong.physics@gmail.com>
2025-03-21 20:28:34 -0400

    scaffolding parallel tempering

--- src/flowMC/strategy/parallel_tempering.py
+++ src/flowMC/strategy/parallel_tempering.py
@@ -1,11 +1,11 @@
 from flowMC.resource.base import Resource
 from flowMC.resource.local_kernel.base import ProposalBase
 from flowMC.resource.buffers import Buffer
+from flowMC.resource.logPDF import LogPDF
 from flowMC.strategy.base import Strategy
 from jaxtyping import Array, Float, PRNGKeyArray
 import jax
 import jax.numpy as jnp
-from typing import Callable
 
 
 class ParallelTempering(Strategy):
@@ -13,17 +13,17 @@ class ParallelTempering(Strategy):
     
     """
 
+    n_temperature: int
 
-    logpdf: Callable[[Float[Array, " n_dim"], dict], Float]
 
     def __init__(self):
-        super().__init__()
+        raise NotImplementedError
 
     def __call__(
         self,
         rng_key: PRNGKeyArray,
         resources: dict[str, Resource],
-        initial_position: Float[Array, "n_chains n_dim"],
+        initial_position: Float[Array, "n_chains n_dims"],
         data: dict,
     ) -> tuple[
         PRNGKeyArray,
@@ -33,8 +33,8 @@ class ParallelTempering(Strategy):
         
         return rng_key, resources, final_position
 
-    def _individal_step(self):
+    def _individal_step(self, positions: Float[Array, "n_temps n_dims"], logpdf: LogPDF):
         raise NotImplementedError
 
-    def _exchange(self):
-        raise NotImplementedError
+    def _exchange(self, positions: Float[Array, "n_temps n_dims"], logpdf: LogPDF):
+        log_prob_temps = jax.vmap(logpdf, in_axes=(0, None))(positions, None) * evaluate_temps(temps_pdf, positions, temperatures)
